8b45f0aad02f8fe7d539c02a8013da95959e561a
Trystan Despierres <despierrestry@yahoo.fr>
2022-03-14 16:46:45 +0100

    Ajout de l'adresse de la base de données

--- connexion.php
+++ connexion.php
@@ -1,7 +1,7 @@
 <?php
 try{
     //Initialisation de la base de données (adresse, utilisateur, mot de passe)
-    $bdd=new PDO("mysql:host;host=localhost;dbname=", "root", "");
+    $bdd=new PDO("mysql:host;host=localhost;dbname=technoweb", "root", "");
     //Permet l'affichage des échanges avec la base de données avec les caractères UTF-8
     $bdd->exec("SET NAMES 'UTF-8'");
 } catch(PDOException $e) {
