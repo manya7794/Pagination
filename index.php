0590e1d8c1bb613c3d55336d44e16d28890f026f
Trystan Despierres <72400479+manya7794@users.noreply.github.com>
2022-03-17 11:10:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ require_once('deconnexion.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Téléversement</title>
 
 </head>
 <body>
