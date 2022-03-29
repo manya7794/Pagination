227bd9b5b76275885482e3ef9f65def08bb33424
Trystan Despierres <despierrestry@yahoo.fr>
2022-03-29 18:14:34 +0200

    Mise a jour

--- LireRecursDir.php
+++ LireRecursDir.php
@@ -1,15 +1,11 @@
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.2.1/themes/default/style.min.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jstree/3.2.1/jstree.min.js"></script>
 <button onclick="window.location.href='index.php';">Retour à l'index</button>
-<div id="jstree_demo_div">
 <P>
 <B>DEBUTTTTTT DU PROCESSUS :</B>
 <BR>
 <?php echo " ", date ("h:i:s"); ?>
 </P>
 <?php
-	echo "<ul>";
+
 //Limite de temps pour l'éxécution du processus
 set_time_limit (500);
 $path= "docs";
@@ -21,9 +17,7 @@ function explorerDir($path)
 {
 	//Ouverture du dossier spécifié
 	$folder = opendir($path);
-	//echo "<p>Ouverture du dossier ". $path."</p>";
-	$dossier = explode("/", $path);
-	echo "<li>".end($dossier)."<ul>";
+	echo "<p>Ouverture du dossier ". $path."</p>";
 	//Tant qu'un fichier est détecté 
 	while($entree = readdir($folder))
 	{		
@@ -47,15 +41,8 @@ function explorerDir($path)
 				//Mise en mémoire du chemin du fichier
 				$path_source = $path."/".$entree;				
 				
-				//echo "<p>Analyse du fichier ". $entree."</p>";
-				//echo "<li>Test".$entree."</li>";
-				echo "<li data-jstree=";
-				echo"'{";
-				echo '"icon":"file.png"';
-				echo "}'";
-				echo '"';
-				echo">".$entree."</li>";
-				
+				echo "<p>Analyse du fichier ". $entree."</p>";
+
 				//Si c'est un .png ou un .jpeg		
 				//Alors je ferais quoi ? Devinez !
 				//...
@@ -66,13 +53,7 @@ function explorerDir($path)
 				
 				if(in_array($extension, $extensions)){
 
-					//echo "<p>".$entree." est une image</p>";
-					echo "<li data-jstree=";
-					echo"'{";
-					echo '"icon":"picture.png"';
-					echo "}'";
-					echo '"';
-					echo">".$entree."</li>";
+					echo "<p>".$entree." est une image</p>";
 
 					//Connexion à la BDD
 					require './connexion.php';
@@ -100,22 +81,8 @@ function explorerDir($path)
 		}
 	}
 	closedir($folder);
-	echo "</ul></li>";
 }
-echo "</ul>";
 ?>
-</div>
-<script>
-                $(function () { $('#jstree_demo_div').jstree(); });
-                $('#jstree_demo_div').on("changed.jstree", function (e, data) {
-                    console.log(data.selected);
-                });
-                $('button').on('click', function () {
-                    $('#jstree').jstree(true).select_node('child_node_1');
-                    $('#jstree').jstree('select_node', 'child_node_1');
-                    $.jstree.reference('#jstree').select_node('child_node_1');
-                });
-            </script>
 <P>
 <B>FINNNNNN DU PROCESSUS :</B>
 <BR>
